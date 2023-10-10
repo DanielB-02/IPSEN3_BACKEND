d4a873b86ed801121abc700dd5e261a9d362ee81
terra-ursus <daniel.d.bor@gmail.com>
2023-10-10 22:24:16 +0200

    Standaard jsession id security ingesteld. Getest met postman, waarbij het probleem optrad dat deze een 401 (unauthorized) ontving. Opgelost door een WebSecurityConfiguration toe te voegen. Het inlog scherm kan nu getoond worden (dus security werkt) en ook kunnen de requests met postman gemaakt worden.

---
+++ damngirl4/src/main/java/com/example/damngirl4/config/WebSecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.damngirl4.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+       http
+               .csrf(AbstractHttpConfigurer::disable)
+               .authorizeHttpRequests((authz) ->
+                       authz.anyRequest().authenticated()
+               )
+               .httpBasic(Customizer.withDefaults());
+       return http.build();
+    }
+}
