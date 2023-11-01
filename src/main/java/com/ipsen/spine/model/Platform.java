be6a5676c7f6134fa15c91297c2008aabe1b860d
s1145204 <1145204@student.hsleiden.nl>
2023-11-01 13:12:11 +0100

    build added testdata for platforms and question

--- src/main/java/com/ipsen/spine/model/Platform.java
+++ src/main/java/com/ipsen/spine/model/Platform.java
@@ -1,14 +1,22 @@
 package com.ipsen.spine.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "platform")
 public class Platform {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String platformName;
+
+
     public Long getId() {
             return id;
         }
--- src/main/java/com/ipsen/spine/model/Question.java
+++ src/main/java/com/ipsen/spine/model/Question.java
@@ -1,8 +1,14 @@
 package com.ipsen.spine.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "question")
 public class Question {
 
