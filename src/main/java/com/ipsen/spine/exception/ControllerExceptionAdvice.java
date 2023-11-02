d9b377ed1d48cd5ef5b01726819e364b6a31c6f8
Emile Schols <s1144096@student.hsleiden.nl>
2023-11-02 20:08:09 +0100

    Added Exception Handling for Bad Credentials Exception that returns 401 Unauthorized.

--- src/main/java/com/ipsen/spine/exception/ControllerExceptionAdvice.java
+++ src/main/java/com/ipsen/spine/exception/ControllerExceptionAdvice.java
@@ -1,6 +1,7 @@
 package com.ipsen.spine.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,10 +10,20 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ControllerExceptionAdvice {
 
     @ExceptionHandler({
+            BadCredentialsException.class
+    })
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorResult notFoundError(BadCredentialsException err) {
+        err.printStackTrace();
+        return new ErrorResult("Invalid email or password.");
+    }
+
+    @ExceptionHandler({
             NotFoundException.class
     })
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResult notFoundError(NotFoundException err) {
+        err.printStackTrace();
         return new ErrorResult(err.getMessage());
     }
 
@@ -21,6 +32,7 @@ public class ControllerExceptionAdvice {
     })
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResult serverError(RuntimeException err) {
+        err.printStackTrace();
         return new ErrorResult(err.getMessage());
     }
 
