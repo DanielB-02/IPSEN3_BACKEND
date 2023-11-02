a10af0e55081d8bac3fec9bbce6e356f1d45566e
Emile Schols <s1144096@student.hsleiden.nl>
2023-11-02 12:12:29 +0100

    Added DatabaseSeeder.

--- src/main/java/com/ipsen/spine/seeder/DatabaseSeeder.java
+++ src/main/java/com/ipsen/spine/seeder/DatabaseSeeder.java
@@ -1,4 +1,4 @@
-package com.ipsen.spine.testdata;
+package com.ipsen.spine.seeder;
 
 import com.ipsen.spine.model.Platform;
 import com.ipsen.spine.model.Question;
@@ -7,47 +7,62 @@ import com.ipsen.spine.model.User;
 import com.ipsen.spine.repository.PlatformRepository;
 import com.ipsen.spine.repository.QuestionRepository;
 import com.ipsen.spine.repository.UserRepository;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.Test;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import static com.ipsen.spine.model.Role.*;
 
-@Component
-@RequiredArgsConstructor
-public class TestDataLoader {
 
-    private final UserRepository userRepository;
-    private final PlatformRepository platformRepository;
-    private final QuestionRepository questionRepository;
-    private final PasswordEncoder passwordEncoder;
-    @Test
-    @PostConstruct
-    private void saveTestData() {
+@Component
+public class DatabaseSeeder {
+
+    private UserRepository userRepository;
+    private PlatformRepository platformRepository;
+    private QuestionRepository questionRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public DatabaseSeeder(UserRepository userRepository, PlatformRepository platformRepository, QuestionRepository questionRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.platformRepository = platformRepository;
+        this.questionRepository = questionRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    private boolean alreadySeeded = false;
+    @EventListener
+    public void seed(ContextRefreshedEvent event){
+        if(alreadySeeded){
+            return;
+        }
+
         createUser("Arie", "Administrator", "admin@spine.com", "12345", ADMIN);
         createUser("Floris", "Ficter", "ficter@spine.com", "12345", FICTER);
         createUser("Robert", "Readonly", "readonly@spine.com", "12345", READONLY);
         Platform platform = createPlatform("x");
         createQuestions("question", platform);
+        this.alreadySeeded = true;
     }
+
+
 
     private void createUser(String firstName, String lastName, String email, String password, Role role) {
         var user = User.builder()
                 .firstName(firstName)
                 .lastName(lastName)
                 .email(email)
-                .password(passwordEncoder.encode(password))
+                .password(this.passwordEncoder.encode(password))
                 .role(role).build();
-            userRepository.save(user);
+        userRepository.save(user);
     }
 
     private Platform createPlatform(String platformName){
         var platform = Platform.builder()
                 .platformName(platformName).build();
-            platformRepository.save(platform);
-            return platform;
+        platformRepository.save(platform);
+        return platform;
     }
 
     private void createQuestions(String textQuestion, Platform platform){
