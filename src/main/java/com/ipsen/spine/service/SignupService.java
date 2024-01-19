e9b67be30a7cdcf8f92a026b835f80080876cc13
DanielB-02 <daniel.d.bor@gmail.com>
2024-01-19 21:57:42 +0100

    Fixed SignupService

--- src/main/java/com/ipsen/spine/service/SignupService.java
+++ src/main/java/com/ipsen/spine/service/SignupService.java
@@ -3,6 +3,7 @@ package com.ipsen.spine.service;
 import com.ipsen.spine.controller.vo.SignUpRequest;
 import com.ipsen.spine.model.Role;
 import com.ipsen.spine.model.User;
+import com.ipsen.spine.repository.RoleRepository;
 import com.ipsen.spine.repository.UserRepository;
 import com.ipsen.spine.security.PermissionBeheerGebruikers;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class SignupService {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
 
     @PermissionBeheerGebruikers
@@ -22,9 +24,8 @@ public class SignupService {
                 .lastName(request.getLastName())
                 .email(request.getEmail())
                 .password(passwordEncoder.encode(request.getPassword()))
-//                .role(request.isFictRole() ? Role.FICTER : Role.READONLY).build();
+                .role(roleRepository.getReferenceById(request.getRoleId()))
                 .build();
         return userRepository.save(user);
     }
-
 }
